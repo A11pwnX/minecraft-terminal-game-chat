7b3e1c850215e1cb29c2de3c69c86958afc0dcde
A11pwnX <a11pwnx@gmail.com>
2025-05-22 06:56:41 +0200

    Update RunnableDemo.java

--- src/main/java/gq/tomatoecraft/RunnableDemo.java
+++ src/main/java/gq/tomatoecraft/RunnableDemo.java
@@ -4,44 +4,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class RunnableDemo implements Runnable{
+import org.bukkit.command.CommandSender;
+import org.bukkit.ChatColor;
+
+public class RunnableDemo implements Runnable {
     private Thread t;
     private String cmd;
-    public RunnableDemo(String cmd){
+    private CommandSender sender;
+
+    public RunnableDemo(String cmd, CommandSender sender) {
         this.cmd = cmd;
+        this.sender = sender;
         start();
     }
-    
-    public void start(){
-        //Initialize the thread
-        if(this.t == null){
+
+    public void start() {
+        if (this.t == null) {
             this.t = new Thread(this);
             this.t.start();
         }
     }
 
-    public void run(){
-        try{
+    public void run() {
+        try {
             Runtime run = Runtime.getRuntime();
-            //Executes the process
             Process process = run.exec(this.cmd);
-            process.waitFor();
 
-            //The output reader of the program
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line = "";
-            while((line = reader.readLine())!=null){
-                System.out.println(line);
+            BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sender.sendMessage(ChatColor.GRAY + line);
             }
-        }
-        catch(InterruptedException e){
-            System.out.println(e.getMessage());
-            System.out.println("Thread interrupted");
-        }
-        catch(IOException e){
-            System.out.println(e.getLocalizedMessage());
-            System.out.println("IOException occured");
+
+            while ((line = errorReader.readLine()) != null) {
+                sender.sendMessage(ChatColor.RED + "error: " + line);
+            }
+
+            process.waitFor();
+        } catch (InterruptedException e) {
+            sender.sendMessage(ChatColor.RED + "Thread stopped: " + e.getMessage());
+        } catch (IOException e) {
+            sender.sendMessage(ChatColor.RED + "execution error: " + e.getLocalizedMessage());
         }
     }
-    
 }
